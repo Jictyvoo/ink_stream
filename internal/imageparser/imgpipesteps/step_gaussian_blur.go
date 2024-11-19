9f65e745ad45000c5f6045a6174d5bbd8671518b
João Victor Oliveira Couto <commits@jictyvoo.dev>
2024-11-19 00:59:47 -0300

    :dromedary_camel: feat: Try a new approach for gaussian blur

--- internal/imageparser/imgpipesteps/step_gaussian_blur.go
+++ internal/imageparser/imgpipesteps/step_gaussian_blur.go
@@ -3,7 +3,6 @@ package imgpipesteps
 import (
 	"image"
 	"image/color"
-	"math"
 
 	"github.com/Jictyvoo/ink_stream/internal/imageparser"
 	"github.com/Jictyvoo/ink_stream/pkg/imgutils"
@@ -13,9 +12,8 @@ var _ imageparser.PipeStep = (*StepApplyGaussianBlurImage)(nil)
 
 type (
 	gaussianKernel struct {
-		radius    int
-		matrix    [][]float64
-		weightSum float64
+		radius  int
+		weights []int
 	}
 	StepApplyGaussianBlurImage struct {
 		kernel gaussianKernel
@@ -24,104 +22,81 @@ type (
 )
 
 func NewStepGaussianBlur(radius int) *StepApplyGaussianBlurImage {
-	var gk gaussianKernel
-	gk.initKernel(radius)
-
-	return &StepApplyGaussianBlurImage{kernel: gk}
+	return &StepApplyGaussianBlurImage{kernel: createBlurKernel(radius)}
 }
 
-func (gk *gaussianKernel) initKernel(radius int) (kernel [][]float64, kSum float64) {
-	gk.radius = radius
-	kernelWidth := (2 * gk.radius) + 1
-	kernel = make([][]float64, kernelWidth)
-	for index := range kernelWidth {
-		kernel[index] = make([]float64, kernelWidth)
+func createBlurKernel(radius int) gaussianKernel {
+	size := (2 * radius) + 1
+	weights := make([]int, size)
+	for i := 0; i <= radius; i++ {
+		weights[i] = 16 * (i + 1)
+		weights[size-1-i] = weights[i]
 	}
-
-	// gaussKernelPoint calculates the weight of a pixel based on its distance
-	gaussKernelPoint := func(distanceSquared float64) float64 {
-		if distanceSquared < 0 {
-			return 0
-		}
-		sigma := max(float64(gk.radius)/2, 1)
-		exponentDenominator := 2 * sigma * sigma
-		return math.Exp(-distanceSquared/(exponentDenominator)) / (2 * math.Pi * sigma * sigma)
-	}
-
-	for dy := -gk.radius; dy <= gk.radius; dy++ {
-		for dx := -gk.radius; dx <= gk.radius; dx++ {
-			distanceSquared := float64((dx * dx) + (dy * dy))
-			kernelValue := gaussKernelPoint(distanceSquared)
-			kernel[dx+gk.radius][dy+gk.radius] = kernelValue
-			kSum += kernelValue
-		}
-	}
-	gk.matrix = kernel
-	gk.weightSum = kSum
-
-	return
+	return gaussianKernel{radius: radius, weights: weights}
 }
 
-func (gk *gaussianKernel) onPoint(x, y int) float64 {
-	return gk.matrix[x][y] / gk.weightSum
-}
-
-// PerformExec applies the Gaussian blur to the entire image
 func (step StepApplyGaussianBlurImage) PerformExec(
 	state *imageparser.PipeState, _ imageparser.ProcessOptions,
-) (err error) {
-	img := state.Img
+) error {
+	if step.kernel.radius <= 0 || step.kernel.radius > 200 {
+		return nil
+	}
 
-	// Create a new image with the same size as the input image
+	img := state.Img
 	bounds := img.Bounds()
 	blurredImg := step.DrawImage(img, bounds)
 
-	// Iterate through each pixel and apply the Gaussian blur
-	for x, y := range imgutils.Iterator(img) {
-		blurredImg.Set(x, y, step.NeighborCalculation(img, x, y))
-	}
-
-	// Update the state with the blurred image
-	state.Img = blurredImg
-	return
-}
-
-// NeighborCalculation calculates the blurred value of a pixel using Gaussian weights
-func (step StepApplyGaussianBlurImage) NeighborCalculation(
-	img image.Image, x, y int,
-) color.Color {
-	var colorSum struct{ red, green, blue, alpha float64 }
-	radius := step.kernel.radius
-	imgBounds := img.Bounds()
-
-	// This is the convolution step
-	// Run the kernel over this grouping of pixels centered around the pixel at (x,y)
-	for dy := -radius; dy <= radius; dy++ {
-		for dx := -radius; dx <= radius; dx++ {
-			px, py := x-dx, y-dy
-			// Ignore loop if is out of bounds
-			if px < imgBounds.Min.X || py < imgBounds.Min.Y ||
-				px > imgBounds.Max.X || py > imgBounds.Max.Y {
-				continue
-			}
-
-			// Get the color values of the current pixel
-			r, g, b, a := img.At(px, py).RGBA()
-			kernelValue := step.kernel.onPoint(dx+radius, dy+radius)
-
-			// Accumulate weighted color values
-			colorSum.red += float64(r>>8) * kernelValue
-			colorSum.green += float64(g>>8) * kernelValue
-			colorSum.blue += float64(b>>8) * kernelValue
-			colorSum.alpha += float64(a>>8) * kernelValue
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			newColor := step.applyKernel(img, x, y, bounds)
+			blurredImg.Set(x, y, newColor)
 		}
 	}
 
-	// Normalize the accumulated values
-	return color.NRGBA{
-		R: imgutils.NormalizePixel(colorSum.red),
-		G: imgutils.NormalizePixel(colorSum.green),
-		B: imgutils.NormalizePixel(colorSum.blue),
-		A: imgutils.NormalizePixel(colorSum.alpha),
+	state.Img = blurredImg
+	return nil
+}
+
+func (step StepApplyGaussianBlurImage) applyKernel(
+	img image.Image, x, y int, bounds image.Rectangle,
+) color.Color {
+	var colorSums struct{ r, g, b, a, wc, wa int64 }
+
+	for wy := 0; wy < len(step.kernel.weights); wy++ {
+		srcY := y + wy - step.kernel.radius
+		if srcY < bounds.Min.Y || srcY >= bounds.Max.Y {
+			continue
+		}
+		for wx := 0; wx < len(step.kernel.weights); wx++ {
+			srcX := x + wx - step.kernel.radius
+			if srcX < bounds.Min.X || srcX >= bounds.Max.X {
+				continue
+			}
+
+			c := img.At(srcX, srcY)
+			r, g, b, a := c.RGBA()
+			wp := int64(step.kernel.weights[wx] * step.kernel.weights[wy])
+
+			colorSums.wa += wp
+			wp *= int64(a) + int64(a>>7)
+			colorSums.wc += wp
+			wp >>= 8
+
+			colorSums.a += wp * int64(a)
+			colorSums.r += wp * int64(r)
+			colorSums.g += wp * int64(g)
+			colorSums.b += wp * int64(b)
+		}
+	}
+
+	if colorSums.wa == 0 || colorSums.wc == 0 {
+		return img.At(x, y) // Fallback to original color
+	}
+
+	return color.RGBA{
+		A: imgutils.NormalizePixel(colorSums.a / colorSums.wa),
+		R: imgutils.NormalizePixel(colorSums.r / colorSums.wc),
+		G: imgutils.NormalizePixel(colorSums.g / colorSums.wc),
+		B: imgutils.NormalizePixel(colorSums.b / colorSums.wc),
 	}
 }
